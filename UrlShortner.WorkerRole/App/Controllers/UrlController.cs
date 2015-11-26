eed9549d31ea989886f5ad0eda12b3568a5ccac0
Krishna Sadasivam <chander.krishna@gmail.com>
2015-11-25 18:05:17 -0800

    Add Hash algorithm and url api controller
    - Add the hash algorithm to compute hash for url string
    - Add the url api controller to handle the Get api call to convert the
    url into short url and store the hash values in a thread safe
    collection.

---
+++ UrlShortner.WorkerRole/App/Controllers/UrlController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+using System.Web.Http;
+using UrlShortner.WorkerRole.App.Utility;
+
+namespace UrlShortner.WorkerRole.App.Controllers
+{
+    public class UrlController : ApiController
+    {
+        private readonly static ConcurrentDictionary<string, string> Dictionary = 
+            new ConcurrentDictionary<string, string>();
+
+        public IHttpActionResult GetUrl(string id)
+        {
+            string urlHash = ComputeHash.GetHashString(id);
+
+            bool isAdded = Dictionary.TryAdd(urlHash, id);
+
+            return Ok(urlHash);
+        }
+
+    }
+}
--- UrlShortner.WorkerRole/App/Controllers/ValuesController.cs
+++ UrlShortner.WorkerRole/App/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ namespace UrlShortner.WorkerRole.App.Controllers
 {
     public class ValuesController : ApiController
     {
-        public IHttpActionResult GetValues()
+        public IHttpActionResult GetValues(string id)
         {
             return Ok(@"{""key1"":""value1"",""key2"":""value2""}");
         }
